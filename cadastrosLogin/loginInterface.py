fc5af0b25df7fb6de5f20bfd7f470585d6f6e35a
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-01-24 21:44:05 -0300

    Update loginInterface.py

--- cadastrosLogin/loginInterface.py
+++ cadastrosLogin/loginInterface.py
@@ -4,14 +4,19 @@ sg.theme('Lightblue')
 
 tela = [    [sg.Text('BEM VINDO(A)')],
             [sg.Text('Digite seu LOGIN: ')],
-            [sg.Input('')],
+            [sg.Input(key='-INPUT-')], #entrada da informação
+            [sg.Text(key='-OUTPUT-')], #saida da informação "print na interface"
             [sg.Button('Criar'), sg.Button('Logar')]
 ]
 
 
 window = sg.Window('Sistema Teste de Login', tela)
-
-event, values = window.read()
-
-if event == 'Criar':
-    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+while True:
+    event, values = window.read()
+    if event == "Criar":
+        window['-OUTPUT-'].update('Hello ' + values['-INPUT-'])
+    if event == sg.WINDOW_CLOSED: #se eu apertar o X da guia ele quebra o laço e fecha a janela
+        break
+    
+    
+window.close()
