821a223d4281dcf58e151c20ef66cb99e952999a
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-02-05 13:36:12 -0300

    Update loginInterface copy.py

--- cadastrosLogin/loginInterface copy.py
+++ cadastrosLogin/loginInterface copy.py
@@ -13,7 +13,7 @@ import PySimpleGUI as sg
 sg.theme('Lightblue')
 tela = [    #oq vai ser mostrado na tela
     [sg.Text('BEM VINDO(A)')],
-    [sg.Text('1-Cadastrar \n2-Login \n')],
+    [sg.Text('1-Cadastrar\n2-Login\n')],
     [sg.Input(key='entrada')], #entrada da informação
     [sg.Text(key='saida')], #saida da informação "print na interface"
     [sg.Button('Confirmar'), sg.Button('Sair')]                           
@@ -30,7 +30,7 @@ while True:
 
 
 
-    
+
     if event == sg.WINDOW_CLOSED: #se eu apertar o X da guia ele quebra o laço e fecha a janela
         break
     
