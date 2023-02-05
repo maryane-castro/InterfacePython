fb7522b8c9ba9d759f2fa78b1ca815dd9d49c531
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-02-05 13:33:47 -0300

    Update loginInterface copy.py

--- cadastrosLogin/loginInterface copy.py
+++ cadastrosLogin/loginInterface copy.py
@@ -1,52 +1,36 @@
 import PySimpleGUI as sg
 
 
-def login():
-    nome = input('Digite o nome de login: ').strip()
-    senha = input('Digite a senha de login: ').strip()
 
-    fixario = open('C:/Users/kookp/OneDrive/Documentos/vscode/InterfacePython/cadastrosLogin/cadastros.txt', 'r')
-    var = fixario.readlines()
-    booleano = bool
-    for i in range(len(var)):
-        num = var[i].split(',')
 
-        if num[0] == nome and num[1].split('\n')[0] :
-            booleano = True
-            if booleano == True:
-                print('Login realizado com sucesso!')
-            break
 
-    
-        
-    else:
-        print('Esse usuário não existe!')
+
 
 
 
 
 
 sg.theme('Lightblue')
-
 tela = [    #oq vai ser mostrado na tela
-
     [sg.Text('BEM VINDO(A)')],
-    [sg.Text('Digite seu LOGIN: ')],
+    [sg.Text('1-Cadastrar \n2-Login \n')],
     [sg.Input(key='entrada')], #entrada da informação
     [sg.Text(key='saida')], #saida da informação "print na interface"
-    [sg.Button('Criar'), sg.Button('Logar')]
-                                   
+    [sg.Button('Confirmar'), sg.Button('Sair')]                           
 ]
 
 
 window = sg.Window('Sistema Teste de Login', tela)
-
-
 while True:
     event, values = window.read()
-    if event == "Criar":
-        login()
-        
+    if event == "1":
+        pass #cadastrar
+    elif event == "1":
+        pass #login
+
+
+
+    
     if event == sg.WINDOW_CLOSED: #se eu apertar o X da guia ele quebra o laço e fecha a janela
         break
     
