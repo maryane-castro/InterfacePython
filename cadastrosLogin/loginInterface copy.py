d255a8ab106abb5438382818275b78ff878d56c7
maryane-castro <122397850+maryane-castro@users.noreply.github.com>
2023-02-15 09:11:17 -0300

    Update loginInterface copy.py

--- cadastrosLogin/loginInterface copy.py
+++ cadastrosLogin/loginInterface copy.py
@@ -1,7 +1,12 @@
 import PySimpleGUI as sg
+ 
 
 
+def telaCadastro():
+    print('tela cadastro')
 
+def telaLogin():
+    print('tela Login')
 
 
 
@@ -16,17 +21,21 @@ tela = [    #oq vai ser mostrado na tela
     [sg.Text('1-Cadastrar\n2-Login\n')],
     [sg.Input(key='entrada')], #entrada da informação
     [sg.Text(key='saida')], #saida da informação "print na interface"
-    [sg.Button('Confirmar'), sg.Button('Sair')]                           
+    [sg.Button('1'), sg.Button('2'), sg.Button('teste')]                           
 ]
 
 
 window = sg.Window('Sistema Teste de Login', tela)
+
+
 while True:
     event, values = window.read()
     if event == "1":
-        pass #cadastrar
-    elif event == "1":
-        pass #login
+        entrada = values['entrada']  #variavel recebe os valores do input
+        print(entrada)
+        telaCadastro()
+    elif event == "2":
+        telaLogin()
 
 
     if event == sg.WINDOW_CLOSED: #se eu apertar o X da guia ele quebra o laço e fecha a janela
@@ -34,3 +43,52 @@ while True:
     
     
 window.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''telaCadastro = [    #oq vai ser mostrado na tela
+    [sg.Text('BEM VINDO(A)')],
+    [sg.Text('1-Cadastrar\n2-Login\n')],
+    [sg.Input(key='entrada')], #entrada da informação
+    [sg.Text(key='saida')], #saida da informação "print na interface"
+    [sg.Button('Confirmar'), sg.Button('Sair')]                           
+]
+
+
+
+
+telaLogin = [    #oq vai ser mostrado na tela
+    [sg.Text('BEM VINDO(A)')],
+    [sg.Text('1-Cadastrar\n2-Login\n')],
+    [sg.Input(key='entrada')], #entrada da informação
+    [sg.Text(key='saida')], #saida da informação "print na interface"
+    [sg.Button('Confirmar'), sg.Button('Sair')]                           
+]'''
